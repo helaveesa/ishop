1d7a7ed510268cad9e2345d438ac75545eef8a99
helaveesa <helaveesa.nolder@gmail.com>
2012-11-19 15:55:24 +0400

    add code to files defmod init ishop rend rout

---
+++ init.lisp
@@ -0,0 +1,19 @@
+(in-package #:strahbroker)
+
+
+(defun menu ()
+    '((:link "/" :title "Главная")
+          (:link "/about/" :title "Страховой-консультант")
+          (:link "/yz/" :title "Яков-Зайдман")
+          (:link "/articles/" :title "Статьи")
+          (:link "/ulinks/" :title "Полезные-ссылки")
+          (:link "/contacts/" :title "Контакты")
+          (:link "/imgcar/" :title "Водителям")))
+
+
+
+;; start
+(restas:start '#:strahbroker :port 2001)
+(restas:debug-mode-on)
+;; (restas:debug-mode-off)
+(setf hunchentoot:*catch-errors-p* t)
