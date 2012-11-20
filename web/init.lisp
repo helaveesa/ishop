61aaf8574253a50ae1655552e8675a82b6fa2277
helaveesa <helaveesa.nolder@gmail.com>
2012-11-20 16:26:13 +0400

    modif port in init.lisp

--- web/init.lisp
+++ web/init.lisp
@@ -1,7 +1,7 @@
 (in-package #:eshop.web)
 
 ;; start
-(restas:start '#:eshop.web :port 8888)
+(restas:start '#:eshop.web :port 8889)
 (restas:debug-mode-on)
 ;; (restas:debug-mode-off)
 (setf hunchentoot:*catch-errors-p* t)
